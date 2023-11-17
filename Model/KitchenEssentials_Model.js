f127c1aeb6d2d3ff8383bcac37cb5a478b4b6c51
FormApi <siddeswarapradeep@gmail.com>
2023-11-17 18:28:46 +0530

    update

--- Model/KitchenEssentials_Model.js
+++ Model/KitchenEssentials_Model.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const kitchenessntialsSchema = new mongoose.Schema({
   name: String,
+  price: Number,
   offer: Number,
+  size: String,
   image: String,
+  quantity: Number
+
 });
 
 const Kitchenessentials = mongoose.model('Kitchenessentials', kitchenessntialsSchema);
